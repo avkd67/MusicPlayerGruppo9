d4c08718296155a712bb3d65d5b05502feddd559
Antonio Vitale <a.vitale132@studenti.unisa.it>
2026-05-29 16:58:00 +0200

    base del progetto con prima interfaccia grafiica funzionante

--- src/main/java/org/example/musicplayergruppo9/HelloApplication.java
+++ src/main/java/org/example/musicplayergruppo9/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/org/example/musicplayergruppo9/fxml/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Music Player \uD83C\uDFB5");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/example/musicplayergruppo9/HelloController.java
+++ src/main/java/org/example/musicplayergruppo9/HelloController.java
@@ -1,14 +1,67 @@
 package org.example.musicplayergruppo9;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private StackPane areaContenuti; // Lo StackPane che fa da schermo per le due view Home e Libreria
+
+    // Metodo collegato al click del tasto "Home" a sinistra
+    @FXML
+    public void mostraHome() {
+        try {
+            // Si carica il file fxml della Home (che sarà un AnchorPane o VBox a sé stante)
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/musicplayergruppo9/fxml/HomeView.fxml"));
+            Node viewHome = loader.load();
+
+            // Si svuota l'area contenuti e inserisci la nuova view
+            areaContenuti.getChildren().setAll(viewHome);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Metodo collegato al click del tasto "Libreria" a sinistra
+    @FXML
+    public void mostraLibreria() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/musicplayergruppo9/fxml/LibreriaView.fxml"));
+            Node viewLibreria = loader.load();
+
+            areaContenuti.getChildren().setAll(viewLibreria);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void play() {
+        System.out.println("Pulsante Play cliccato!");
+    }
+
+    @FXML
+    public void skipSong() {
+        System.out.println("Pulsante skip cliccato!");
+    }
+
+    @FXML
+    public void skipPlaylist() {
+        System.out.println("Pulsante skipPlaylist cliccato!");
+    }
+
+    @FXML
+    public void loopSong() {
+        System.out.println("Pulsante loop cliccato!");
+    }
+
+    @FXML
+    public void unDo() {
+        System.out.println("Pulsante unDo cliccato!");
     }
 }
